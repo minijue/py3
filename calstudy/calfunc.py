6fe9cb4abc251fe188587daf277e6e0462d0293d
王珏 <jue952@126.com>
2019-03-19 20:08:38 +0800

    1. 包含0的算式不超过5个 2. 减法和除法中，操作数相同的算式不超过2个 3. 修改了每行算式可能小于5个的bug

--- calstudy/calfunc.py
+++ calstudy/calfunc.py
@@ -1,25 +1,65 @@
 import random
 
+if __name__ == '__main__':
+    print("错误！不能执行库文件")
+    exit(-1)
+
+n0 = 0  # 统计有0算式的数量
+ns = 0  # 减法和除法中统计两个操作数相等的数量
 
 def ranplus():
-    a = random.randint(0, 99)
-    b = random.randint(0, 99)
-    return f'{a:2d} + {b:2d} ='
+    a = 0
+    b = 0
+    global n0
+    start = 1 if n0 > 5 else 0  # 有0的算式不能超过5个
+    while a == b == 0:  # 都是0无意义
+        a = random.randint(start, 99)
+        b = random.randint(start, 99)
+        while a + b > 100:  # 和不大于100
+            b = random.randint(start, 99)
+    if a == 0 or b == 0:
+        n0 += 1
+    return f'{a} + {b} ='
 
 
 def ransub():
-    a = random.randint(1, 100)
-    b = random.randint(0, a)
-    return f'{a:2d} - {b:2d} ='
+    a = 0
+    b = 0
+    global n0, ns
+    start = 1 if n0 > 5 else 0  # 有0的算式不能超过5个
+    while a == b and (ns > 2 or b == 0):  # 操作数都为0或者相同的次数超过限制
+        a = random.randint(start, 100)
+        b = random.randint(start, a)
+    if a == 0 or b == 0:
+        n0 += 1
+    if a == b:
+        ns += 1
+    return f'{a} - {b} ='
 
 
 def ranmul():
-    a = random.randint(0, 9)
-    b = random.randint(0, 9)
-    return f'{a:2d} × {b:<2d} ='
+    a = 0
+    b = 0
+    global n0
+    start = 1 if n0 > 5 else 0  # 有0的算式不能超过5个
+    while a == b == 0:  # 都是0无意义
+        a = random.randint(start, 9)
+        b = random.randint(start, 9)
+    if a == 0 or b == 0:
+        n0 += 1
+    return f'{a} × {b} ='
 
 
 def randivintbl():
-    a = random.randint(1, 9)
-    b = random.randint(0, 9)
-    return f'{a * b:2d} ÷ {a:<2d} ='
+    a = 0
+    b = 0
+    global n0, ns
+    start = 1 if n0 > 5 else 0  # 有0的算式不能超过5个
+    while a == b and (ns > 2 or a == 0):  # 操作数都为0或者相同的次数超过限制
+        a = random.randint(1, 9)
+        b = random.randint(start, 9)
+    if b == 0:
+        n0 += 1
+    if a == b:
+        ns += 1
+    return f'{a * b:} ÷ {a} ='
--- calstudy/calmain.py
+++ calstudy/calmain.py
@@ -1,30 +1,52 @@
-import calfunc
 import random
+import time
 
-from reportlab.pdfgen import canvas
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+
+import calfunc
 
 n = int(input('Please input the number:'))
 if n > 0:
-    x = 1
-    y = 1
-    p = canvas.Canvas('kousuan.pdf')
+    t = time.localtime()
+    # 以时间和次数组合成文件名
+    filename = '{:4d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}({}).pdf'.format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour,
+                                                                         t.tm_min, t.tm_sec, n)
+    pdf = SimpleDocTemplate(filename,
+                            pagesize=(A4[0], A4[1]),
+                            rightMargin=20,
+                            leftMargin=20,
+                            topMargin=20,
+                            bottomMargin=20)
+    t = set()
+    elements = []
+
     for i in range(n):
         tp = random.randint(1, 4)
         if tp == 1:  # plus
-            p.drawString(x, y, calfunc.ranplus())
+            t.add(calfunc.ranplus())
         elif tp == 2:  # subtract
-            p.drawString(x, y, calfunc.ransub())
+            t.add(calfunc.ransub())
         elif tp == 3:  # multiply
-            p.drawString(x, y, calfunc.ranmul())
+            t.add(calfunc.ranmul())
         elif tp == 4:  # divide
-            p.drawString(x, y, calfunc.randivintbl())
+            t.add(calfunc.randivintbl())
         else:
             print('error')
             break
-        x += 70
-        if (i + 1) % 4 == 0:
-            y += 30
-            x = 1
+        if len(t) % 5 == 0:
+            elements.append(list(t))
+            t.clear()
 
-    p.showPage()
-    p.save()
+    etable = Table(elements, rowHeights=38, colWidths=110)
+    etable.setStyle(
+        TableStyle([
+            ('FONTSIZE', (0, 0), (-1, -1), 14),  # 字体大小
+            ('ALIGN', (-1, 0), (-2, 0), 'LEFT'),  # 对齐
+            ('TEXTCOLOR', (0, 1), (-2, -1), colors.black),  # 设置表格内文字颜色
+            ('GRID', (0, 0), (-1, -1), 0, colors.white),  # 设置表格框线
+        ])
+    )
+    contents = [etable]
+    pdf.build(contents)
